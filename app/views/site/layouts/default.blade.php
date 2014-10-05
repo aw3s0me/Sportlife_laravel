ec069097c70f8debda801c3977c1ecac0adb86e9
aw3s0me <aw3s0me@rocketmail.com>
2014-10-05 20:12:04 +0400

    Added bootstrap, blog + admin panel + header

---
+++ app/views/site/layouts/default.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Basic Page Needs
+		================================================== -->
+		<meta charset="utf-8" />
+		<title>
+			@section('title')
+			Sportlife
+			@show
+		</title>
+		@section('meta_keywords')
+		<meta name="keywords" content="sport, blog, network" />
+		@show
+		@section('meta_author')
+		<meta name="author" content="Aleksander Korovin" />
+		@show
+		@section('meta_description')
+		<meta name="description" content="Social network for people loving sport way of life" />
+                @show
+		<!-- Mobile Specific Metas
+		================================================== -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- CSS
+		================================================== -->
+        <link href=' http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
+        <!-- <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap-theme.min.css')}}">-->
+        <link rel="stylesheet" href="{{asset('assets/css/master.css')}}">
+		<style>
+        body {
+            padding: 60px 0;
+        }
+		@section('styles')
+		@show
+		</style>
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Favicons
+		================================================== -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{{ asset('assets/ico/apple-touch-icon-144-precomposed.png') }}}">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{{ asset('assets/ico/apple-touch-icon-114-precomposed.png') }}}">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}}">
+		<link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}}">
+		<link rel="shortcut icon" href="{{{ asset('assets/ico/favicon.png') }}}">
+	</head>
+
+	<body>
+		<!-- To make sticky footer need to wrap in a div -->
+		<div id="wrap">
+		<!-- Navbar -->
+		<div class="my-header-inverse navbar navbar-default navbar-inverse navbar-fixed-top">
+			 <div class="my-header container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+						<li class="logo" {{ (Request::is('/') ? ' class="active"' : '') }}><a href="{{{ URL::to('') }}}">SPORTLIFE</a></li>
+                        <li><a href="{{{ URL::to('users') }}}">Users</a></li>
+                        <li><a href="{{{ URL::to('groups') }}}">Groups</a></li>
+                        <li><a href="{{{ URL::to('orgs') }}}">Organizations</a></li>
+					</ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                          <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                      </form>
+                    <ul class="nav navbar-nav pull-right">
+                        @if (Auth::check())
+                        @if (Auth::user()->hasRole('admin'))
+                        <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
+                        @endif
+                        <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
+                        <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
+                        @else
+                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
+                        <li {{ (Request::is('user/create') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
+                        @endif
+                    </ul>
+					<!-- ./ nav-collapse -->
+				</div>
+			</div>
+		</div>
+		<!-- ./ navbar -->
+
+		<!-- Container -->
+		<div class="container">
+			<!-- Notifications -->
+			@include('notifications')
+			<!-- ./ notifications -->
+
+			<!-- Content -->
+			@yield('content')
+			<!-- ./ content -->
+		</div>
+		<!-- ./ container -->
+
+		<!-- the following div is needed to make a sticky footer -->
+		<div id="push"></div>
+		</div>
+		<!-- ./wrap -->
+
+
+	    <div id="footer">
+	      <div class="container">
+	        <p class="muted credit">Made by Korovin Aleksander<a href="https://github.com/aw3s0me">Github</a>.</p>
+	      </div>
+	    </div>
+
+		<!-- Javascripts
+		================================================== -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+
+        @yield('scripts')
+	</body>
+</html>
